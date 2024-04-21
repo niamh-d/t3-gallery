afab49c6adb504e35abe043b9f761c213bef8115
Niamh <niamh.linguist@gmail.com>
2024-04-21 17:34:47 +0300

    added delete

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -2,6 +2,10 @@ import { auth } from "@clerk/nextjs/server";
 import "server-only";
 
 import { db } from "~/server/db";
+import { images } from "./db/schema";
+import { and, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function getMyImages() {
   const user = auth();
@@ -30,4 +34,15 @@ export async function getImage(id: number) {
   if (image.userId !== user.userId) throw new Error("Unauthorized");
 
   return image;
+}
+
+export async function deleteImage(id: number) {
+  const user = auth();
+  if (!user.userId) throw new Error("Unauthorized");
+
+  await db
+    .delete(images)
+    .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+  redirect("/");
 }
