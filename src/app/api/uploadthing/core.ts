941a91daab93923743543b84b8e86be649da6cad
Niamh <niamh.linguist@gmail.com>
2024-04-23 00:17:53 +0300

    upped upload limit from 8 -> 10

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -12,7 +12,7 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 6 } })
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 10 } })
     // Set permissions and file types for this FileRoute
     .middleware(async () => {
       // This code runs on your server before upload
@@ -28,7 +28,7 @@ export const ourFileRouter = {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
 
-      console.log(uuidv4())
+      console.log(uuidv4());
 
       await db.insert(images).values({
         name: file.name,
